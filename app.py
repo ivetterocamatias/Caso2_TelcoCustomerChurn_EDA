53a69eaf0b9133b620778f7178b7986d327bf208
ivetterocamatias <ivetterocamatias@gmail.com>
2026-08-25 02:01:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -23,10 +23,60 @@ col1, col2, col3 = st.columns([1,2,1])
 
 with col2:
     st.sidebar.image("Teleco.png", width=600)
-    
-modulo = st.sidebar.selectbox(
-    "Menú",
-    ("Home", "Ejercicio 1", "Ejercicio 2", "Ejercicio 3", "Ejercicio 4")
+
+st.sidebar.title("Menú principal")
+
+opcion = st.sidebar.radio(
+    "Selecciona una sección:",
+    [
+        "🏠 Home",
+        "📂 Carga del dataset",
+        "📊 Análisis Exploratorio",
+        "📝 Conclusiones"
+    ]
 )
 
 # Información en sección Home
+
+if modulo == "🏠 Home":
+
+    st.header("Telco Customer Churn: Análisis Exploratorio de Datos")
+    
+    st.subheader("""
+    
+    ### Breve descripción del objetivo del análisis
+
+    Esta aplicación tiene como objetivo realizar un Análisis
+    Exploratorio de Datos (EDA) del dataset Telco Customer Churn,
+    con el propósito de identificar patrones asociados a la fuga
+    de clientes.
+    """)
+
+    st.subheader("Datos generales de autora")
+
+    st.write("""
+    **Nombre completo:** Ivette Isaura Roca Matias
+    
+    **Especialización:** Especialización en Python for Analytics
+    
+    **Información general de la estudiante:** Ingeniera pesquera cursando una maestría en Ciencia y Tecnología de Alimentos
+    
+    **Año:** 2026
+    """)
+
+     st.subheader("Sobre el dataset")
+
+    st.write(
+        "El dataset contiene información sobre clientes de una empresa
+        de telecomunicaciones, incluyendo características demográficas,
+        servicios contratados, permanencia, facturación y estado de churn.
+    )
+    
+    st.markdown("""
+    ### Tecnologías utilizadas
+
+    - 🐍 Python
+    - 👑 Streamlit
+    - 📊 NumPy
+    - 🐼 Pandas
+    """)
