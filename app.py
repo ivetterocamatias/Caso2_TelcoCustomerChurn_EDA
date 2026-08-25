1d8d92ca51169a223010a5a87c7cebdb4a2ccbda
ivetterocamatias <ivetterocamatias@gmail.com>
2026-08-25 01:21:40 -0300

    Enhance layout with images and sidebar menu
    Added images to the main layout and sidebar for better visual presentation.

--- app.py
+++ app.py
@@ -12,3 +12,21 @@ st.set_page_config(
 )
 
 st.title("Telco Customer Churn — Análisis Exploratorio de Datos")
+col1, col2, col3 = st.columns([1,2,1])
+
+with col2:
+    st.image("Telco_logo.png", width=600)
+
+# Menú lateral
+
+col1, col2, col3 = st.columns([1,2,1])
+
+with col2:
+    st.sidebar.image("Teleco.png", width=600)
+    
+modulo = st.sidebar.selectbox(
+    "Menú",
+    ("Home", "Ejercicio 1", "Ejercicio 2", "Ejercicio 3", "Ejercicio 4")
+)
+
+# Información en sección Home
